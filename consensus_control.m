b629a59101d6bdc555b6f3f34ce688dc12722b0d
Melih yıldırım <melih.2000hs@gmail.com>
2024-03-16 01:31:47 +0300

    init for multiagent variables

--- consensus_control.m
+++ consensus_control.m
@@ -2,3 +2,12 @@ clc;
 close all;
 clear;
 
+time = 0:.1:20; 
+%% initialize velocity, position and heading
+V=10; % velocity
+InitHeadings=rand(6,1)*2*pi; % initialize random heading for 6 nodes
+
+Xpositions=zeros(6,1);% x position for 6 nodes,it will contains all positions
+Ypositions=zeros(6,1);% y position for 6 nodes,it will contains all positions
+
+IC =[InitHeadings Xpositions Ypositions];
